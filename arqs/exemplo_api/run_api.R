e5a4126e975d9c8ae6014a98cd719023635acb8a
Athos Damiani <athos.damiani@gmail.com>
2021-06-29 16:51:47 -0300

    ASS

--- arqs/exemplo_api/run_api.R
+++ arqs/exemplo_api/run_api.R
@@ -1,6 +1,9 @@
+# library(plumber)
+#
+# api <- plumb("arqs/exemplo_api/exemplo_api.R")
+# pr_run(api, port = 8000)
+#
+#
 library(plumber)
-
-api <- plumb("arqs/exemplo_api/exemplo_api.R")
-pr_run(api, port = 8000)
-
-
+r <- plumb("exemplo_api.R")
+r$run(port = 8000, host = "0.0.0.0")
