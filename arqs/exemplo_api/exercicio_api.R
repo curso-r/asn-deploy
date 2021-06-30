be46bffefd7bbab7533d2b377669040eaeb0ba83
Athos Damiani <athos.damiani@gmail.com>
2021-06-30 18:51:39 -0300

    asd

--- arqs/exemplo_api/exercicio_api.R
+++ arqs/exemplo_api/exercicio_api.R
@@ -1,4 +1,5 @@
 # Exercício de plumber
+
 # 0) combo usethis para criar projetos
 # 1) crie um arquivo endpoints.R
 # 2) crie um endpoint GET que receba um argumento chamado obj
@@ -10,4 +11,13 @@
 # ML -> plumber -> dockerfile -> github ----> GATILHO -> Cloud Build ---
 
 # -> colocar na VM ----> GET()
+
+
+# Exercício de Docker
+# Criar um Dockerfile
+# - use a imagem rstudio/plumber
+# - copie os dois arquivos do plumber (o dos endpoints e o do plumb) para o container
+# - exponha a porta 8000
+# - faça o comando padrão rodar o arquivo que faz o plumb. DICA: o handler é o Rscript
+
 
