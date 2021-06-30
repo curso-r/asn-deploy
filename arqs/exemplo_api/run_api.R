0fd0252b67b3e6876d4f7cb93ef666a60a4fca4b
Athos Damiani <athos.damiani@gmail.com>
2021-06-30 12:57:13 -0300

    asd

--- arqs/exemplo_api/run_api.R
+++ arqs/exemplo_api/run_api.R
@@ -1,4 +1,9 @@
 
 library(plumber)
 r <- plumb("exemplo_api.R")
+
+p$handle("GET", "/", function(req, res){
+  200
+})
+
 r$run(port = as.integer(Sys.getenv("PORT", unset = 8000)), host = "0.0.0.0")
