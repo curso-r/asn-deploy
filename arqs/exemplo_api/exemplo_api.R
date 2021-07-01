2d27b09196f0d79886e62cd1279f3ae1b4b44214
Athos Damiani <athos.damiani@gmail.com>
2021-06-30 22:10:56 -0300

    só pra disparar o gatilho

--- arqs/exemplo_api/exemplo_api.R
+++ arqs/exemplo_api/exemplo_api.R
@@ -7,7 +7,7 @@
 #* @param titulo o titulo
 #* @get /echo
 function(msg = "", titulo = "") {
-  paste0("A mensagem é DRI BOANOITE!!!: '", msg, "'")
+  paste0("A mensagem é DRI BOITE!!!: '", msg, "'")
 }
 
 
