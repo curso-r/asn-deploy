49afc262076a876b4f0819a8d13971e38a7b87d0
Athos Damiani <athos.damiani@gmail.com>
2021-07-05 18:20:45 -0300

    asd

--- arqs/exemplo_api/exercicio_api.R
+++ arqs/exemplo_api/exercicio_api.R
@@ -1,26 +1,29 @@
-# Exercício de plumber
+# Parte1) Exercício de plumber -------------------------------------
+1) combo usethis para criar projetos (create_project(), use_git(), use_github() ...)
+2) crie um arquivo endpoints.R
+3) crie um endpoint GET que receba um argumento chamado obj
+4) "Decore" a documentação com #* iformando os parâmetros e o Título.
+5) crie um arquivo run.R
+6) crie um código para inicializar a api do endpoints.R
+teste usando httr::GET() e httr::content() (pode testar no navegador também)
 
-# 0) combo usethis para criar projetos (usethis::use_git() ...)
-# 1) crie um arquivo endpoints.R
-# 2) crie um endpoint GET que receba um argumento chamado obj
-# 3) "Decore" a documentação com #* iformando os parâmetros e o Título.
-# 4) crie um arquivo run.R
-# 5) crie um código para inicializar a api do endpoints.R
-# teste usando httr::GET() e httr::content() (pode testar no navegador também)
+# Parte 2) Exercício de Docker ---------------------------------------
+Criar um Dockerfile
+- (FROM) use a imagem rstudio/plumber
+- (COPY) copie os dois arquivos do plumber (o dos endpoints e o do plumb) para o container
+- (EXPORT) exponha a porta 8080
+- (CMD) faça o comando padrão rodar o arquivo que faz o plumb. DICA: o handler é o Rscript
 
-# ML -> plumber -> dockerfile -> github ----> GATILHO -> Cloud Build ---
+# Parte 3) Exercício Deploy ------------------------------------------
+Jogar os arquivos pro Github
+Criar um Cloud Run no google cloud platform
+Testar se está funcionando
 
-# -> colocar na VM ----> GET()
 
 
-# Exercício de Docker
-# Criar um Dockerfile
-# - (FROM) use a imagem rstudio/plumber
-# - (COPY) copie os dois arquivos do plumber (o dos endpoints e o do plumb) para o container
-# - (EXPORT) exponha a porta 8080
-# - (CMD) faça o comando padrão rodar o arquivo que faz o plumb. DICA: o handler é o Rscript
 
-# Exercício Deploy
-# Jogar os arquivos pro Github
-# Criar um Cloud Run
-# Testar se está funcionando
+# DICA: visite https://github.com/curso-r/asn-deploy para inspirações =P
+
+# Linhas gerais
+ML(codigo) -> plumber -> dockerfile -> github ----> GATILHO -> Cloud Build ---
+-> colocar no Cloud Run ----> GET()
--- arqs/exemplo_api/run_api.R
+++ arqs/exemplo_api/run_api.R
@@ -6,6 +6,4 @@ r$handle("GET", "/", function(req, res){
   2000
 })
 
-
-
 r$run(port = as.integer(Sys.getenv("PORT", unset = 8000)), host = "0.0.0.0")
