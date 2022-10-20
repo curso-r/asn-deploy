7b1fdc42f2d7af1282875c5531943b71af09a688
Athos Petri Damiani <1925102+Athospd@users.noreply.github.com>
2022-10-20 19:29:55 -0300

    Update exemplo_rscript.R

--- arqs/exemplo_rscript.R
+++ arqs/exemplo_rscript.R
@@ -1,6 +1,7 @@
 # comentario para exemplo de merge entre dois repos
 # o professor alterou exatamente o mesmo arquivo que o Juan
 # quero mais um coment
+# quero mais um coment3
 args <- commandArgs(TRUE)
 
 print(args)
